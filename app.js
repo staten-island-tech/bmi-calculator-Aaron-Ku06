372d4385b020b704d4afbe63d997b88d95ebe857
Aaron-Ku06 <aaronku320@gmail.com>
2023-10-04 09:24:55 -0400

    Working BMI

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
-function first(a, b){
-    console.log((a / (b**2)))
+function BMI (){
+    console.log(730*(a/b**2));
 }
-// need to convert the String to a Number
 let a = Number(prompt("Enter Your Weight"));
 let b = Number(prompt("Enter Your Height"));
-const x = divide(a, b ** 2);
-const p = 730;
-
-function BMI(p, x){
-    console.log(x * p);
-}
-multiply(x, p)
+BMI(730*(a/b**2));

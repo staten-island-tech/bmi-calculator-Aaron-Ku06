67cccfa24e2aa6d3f839d157b6ce9a2479133b4a
Aaron-Ku06 <aaronku320@gmail.com>
2023-10-05 09:23:43 -0400

    Intervals

--- app.js
+++ app.js
@@ -4,3 +4,19 @@ function BMI (){
 let a = Number(prompt("Enter Your Weight"));
 let b = Number(prompt("Enter Your Height"));
 BMI(730*(a/b**2));
+
+function check(BMI){
+    if (BMI <= 18.5){
+        console.log("Eat a couple hamburgers")
+    } else if (BMI > 18.5 && BMI <= 24.5){
+        console.log("No need for hamburgers");
+    } else if (BMI > 24.5 && BMI <= 29.5){
+        console.log("Stop eating hamburgers");
+    } else if (BMI > 29.5 && BMI <= 35){
+        console.log("Eat a salad or become hamburger");
+    } else if (BMI > 35){
+        console.log("You are literally the hamburger");
+    } 
+    return(console.log);
+};
+

52b267d3b9c83d6803e5b9ed0e4f015d2a7e6dca
Aaron-Ku06 <aaronku320@gmail.com>
2023-10-06 09:22:14 -0400

    Returns

--- app.js
+++ app.js
@@ -7,16 +7,18 @@ BMI(730*(a/b**2));
 
 function check(BMI){
     if (BMI <= 18.5){
-        console.log("Eat a couple hamburgers")
+        return "Eat a couple hamburgers"
     } else if (BMI > 18.5 && BMI <= 24.5){
-        console.log("No need for hamburgers");
+        return "No need for hamburgers"
     } else if (BMI > 24.5 && BMI <= 29.5){
-        console.log("Stop eating hamburgers");
+        return "Stop eating hamburgers"
     } else if (BMI > 29.5 && BMI <= 35){
-        console.log("Eat a salad or become hamburger");
+        return "Eat a salad or become hamburger"
     } else if (BMI > 35){
-        console.log("You are literally the hamburger");
+        return "You are literally the hamburger"
     } 
-    return(console.log);
 };
+let result = check(BMI)
+console.log(results)
+
 

9a1b1cf73d65119b57081e109033519f17d96ef4
Aaron-Ku06 <aaronku320@gmail.com>
2023-10-10 09:17:40 -0400

    Finished Assignment

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
-function BMI (){
-    console.log(730*(a/b**2));
+function getBMI (a,b){
+    console.log(a / ((b/100)**2));
+    console.log(N);
+    return (a / ((b/100)**2));
 }
 let a = Number(prompt("Enter Your Weight"));
 let b = Number(prompt("Enter Your Height"));
-BMI(730*(a/b**2));
-
-function check(BMI){
-    if (BMI <= 18.5){
-        return "Eat a couple hamburgers"
-    } else if (BMI > 18.5 && BMI <= 24.5){
-        return "No need for hamburgers"
-    } else if (BMI > 24.5 && BMI <= 29.5){
-        return "Stop eating hamburgers"
-    } else if (BMI > 29.5 && BMI <= 35){
-        return "Eat a salad or become hamburger"
-    } else if (BMI > 35){
-        return "You are literally the hamburger"
+let N = prompt("Enter your name");
+WBMI = getBMI(a,b);
+function check(){
+    if (WBMI <= 18.5){
+        console.log("Eat a couple hamburgers");
+    } else if (WBMI > 18.5 && WBMI <= 24.5){
+        copnsole.log("No need for hamburgers");
+    } else if (WBMI > 24.5 && WBMI <= 29.5){
+        console.log("Stop eating hamburgers");
+    } else if (WBMI > 29.5 && WBMI <= 35){
+        console.log("Eat a salad or become hamburger");
+    } else if (WBMI > 35){
+        console.log("You are literally the hamburger");
     } 
-};
-let result = check(BMI)
-console.log(results)
+}
+check(WBMI)
+
 
 
